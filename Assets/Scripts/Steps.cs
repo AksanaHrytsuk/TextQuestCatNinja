e89a5a90ecbac113b5f801c04dc2abe3381d4cc5
AksanaHrytsuk <oksana@redeyes.homenetwork>
2020-02-11 05:18:43 +0300

    Add Homework

---
+++ Assets/Scripts/Steps.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Steps : MonoBehaviour
+{
+  [TextArea]
+  public string content;
+  [TextArea]
+  public string choices;
+  public Steps[] steps;
+  public int karmaMod;
+
+}
---
+++ Assets/Scripts/TextQuestCatNinja.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextQuestCatNinja : MonoBehaviour
+{
+
+  public int karmaCount;
+  [Header("Elements")]
+  public Text karma;
+  public Text contentText; // переменная отвечает за историю
+  public Text choicesText; // переменная отвечает за вывод вариантов
+  public Steps activeStep; // степ, выводящийся на экран в данный момент
+  public Steps instruction;
+  public Steps gameOver;
+
+  void ResetGame(Steps any)
+  {
+    karmaCount = 2;
+    contentText.text = any.content;
+    choicesText.text = any.choices;
+    karma.text = karmaCount.ToString();
+    activeStep = any.steps[0];
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    ResetGame(activeStep);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Alpha1))
+    {
+      checkPress(0);
+    }
+    else if (Input.GetKeyDown(KeyCode.Alpha2))
+    {
+      checkPress(1);
+    }
+    else if (Input.GetKeyDown(KeyCode.Alpha3))
+    {
+      checkPress(2);
+    }
+    else if (Input.GetKeyDown(KeyCode.Escape))
+    {
+      ResetGame(instruction);
+    }
+  }
+  void checkPress(int index)
+  {
+    if (index < activeStep.steps.Length)
+    {
+      activeStep = activeStep.steps[index];
+      contentText.text = activeStep.content;
+      choicesText.text = activeStep.choices;
+      karmaCount += activeStep.karmaMod;
+      karma.text = karmaCount.ToString();
+      if (karmaCount == 0)
+      {
+        ResetGame(gameOver);
+      }
+    }
+  }
+
+}
